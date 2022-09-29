b89512f60db6554066ad6bb58be51a71bbea1b11
luc0liv <luoliveiradacunha@gmail.com>
2022-09-29 20:28:30 -0300

    Requisito 5: parte 2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,15 +2,20 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (!animal) {
-    const species = data.species.reduce((acc, curr) => {
-      acc[curr.name] = curr.residents.length;
-      return acc;
+    const species = data.species.reduce((acc, currObj) => {
+      const newObj = acc;
+      newObj[currObj.name] = currObj.residents.length;
+      return newObj;
     }, {});
 
     return species;
   }
+
+  const getBySpecies = data.species.find((ani) => ani.name === animal.specie).residents.length;
+
+  return getBySpecies;
 }
 
-console.log(countAnimals());
+// console.log(countAnimals({ specie: 'lions', sex: 'female' }));
 
 module.exports = countAnimals;
