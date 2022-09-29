6a2f2be99cefb61fb0311831d35def31858e8e31
luc0liv <luoliveiradacunha@gmail.com>
2022-09-29 15:50:18 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+
+  return data.species.filter((spec) => ids.some((id) => spec.id === id));
 }
 
 module.exports = getSpeciesByIds;
