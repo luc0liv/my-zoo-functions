bdb1e2550d9fdb2ffa3bfee342a85927b95fa975
luc0liv <luoliveiradacunha@gmail.com>
2022-09-29 17:13:57 -0300

    Requisito 4: função isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,13 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managersIds = [stephanieId, olaId, burlId];
+
+const isManager = (id) => managersIds.some((manager) => manager === id);
+
+// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
