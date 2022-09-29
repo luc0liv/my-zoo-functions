f33fb75e6c86bba01bb7600b54037ff8ab54ed61
luc0liv <luoliveiradacunha@gmail.com>
2022-09-29 16:51:16 -0300

    Requisito 3: parte 1

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  if (!employeeName) {
+    return {};
+  }
+};
 
 module.exports = getEmployeeByName;
