791f098a29a906aaa3272990bc478984fe4cc32e
luc0liv <luoliveiradacunha@gmail.com>
2022-09-29 16:43:14 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalFiltered = data.species.filter((spec) => spec.name === animal);
+  const [species] = animalFiltered;
+  return species.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
