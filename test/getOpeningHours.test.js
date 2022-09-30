0155efca1e5b968b88e85bdc4fa8daf7a026b8a0
luc0liv <luoliveiradacunha@gmail.com>
2022-09-30 01:13:27 -0300

    Requisito 16 (?)

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -40,4 +40,12 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem:  "The minutes should represent a number"', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
   });
+
+  it('Para os argumentos Monday e 13:00-AM deve lançar uma exceção com a mensagem:  "The hour must be between 0 and 12"', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow('The hour must be between 0 and 12');
+  });
+
+  it('Para os argumentos Tuesday e 09:60-AM deve lançar uma exceção com a mensagem: "The minutes must be between 0 and 59"', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow('The minutes must be between 0 and 59');
+  });
 });
