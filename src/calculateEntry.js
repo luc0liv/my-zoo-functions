7729702be906bce9b9c551cc74dc615c64f2d559
luc0liv <luoliveiradacunha@gmail.com>
2022-09-30 18:36:53 -0300

    Requisito 8: função countEntrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const entrantsArray = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const child = entrants.filter((entrant) => entrant.age >= 0 && entrant.age < 18);
+
+  const entries = { child: child.length, adult: adult.length, senior: senior.length };
+
+  return entries;
+};
+
+console.log(countEntrants(entrantsArray));
+
+const calculateEntry = (entrants) => {
+};
 
 module.exports = { calculateEntry, countEntrants };
