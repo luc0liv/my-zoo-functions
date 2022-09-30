d7588634cc1af8b57a0d9e168159466f39379f2f
luc0liv <luoliveiradacunha@gmail.com>
2022-09-30 01:06:04 -0300

    Requisito 13 :star:

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,4 +24,20 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Wednesday e 09:00-PM deve retornar a string "The zoo is closed"', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
+
+  it('Para os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: "The day must be valid."', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+  });
+
+  it('Para os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: "The abbreviation must be \'AM\' or \'PM\'"', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+
+  it('Para os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem: "The hour should represent a number"', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+  });
+
+  it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem:  "The minutes should represent a number"', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+  });
 });
