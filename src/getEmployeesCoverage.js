acb5ba2a99cdd37a206f3e48c6a54c460b7112a9
luc0liv <luoliveiradacunha@gmail.com>
2022-10-02 17:07:41 -0300

    Requisito 11:  retornando obj com infos da pessoa passada no param

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const getEmployeesCoverage = (person) => {
+  if (person.name || person.id) {
+    const employee = employees
+      .find((emp) => emp.firstName === person.name
+      || emp.lastName === person.name
+      || emp.id === person.id);
+    const { id, firstName, lastName, responsibleFor } = employee;
+    const getSpecies = species.filter((spec) => responsibleFor.some((el) => spec.id === el));
+    return {
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: getSpecies.map((sp) => sp.name),
+      locations: getSpecies.map((sp) => sp.location),
+    };
+  }
+};
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
