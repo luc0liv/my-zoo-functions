c2efe0d178dbfa988f2c27a42940dc4dea63f1d2
luc0liv <luoliveiradacunha@gmail.com>
2022-10-03 22:37:01 -0300

    Requisito 9: testando segunda parte com hard code

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { species, hours } = data;
+
+const getSchedule = (scheduleTarget) => {
+  if (!scheduleTarget) {
+    return {
+      Tuesday: { // Dia da semana
+        officeHour: `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm.`, // n
+        exhibition: species.filter((spec) => spec.availability.includes('Tuesday')).map((spec) => spec.name),
+      },
+      Wednesday: {
+        officeHour: 'Open from 8am until 6pm',
+        exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+      },
+    };
+  }
+  const getAnimalSchedule = species
+    .find((spec) => spec.name === scheduleTarget).availability;
+  return getAnimalSchedule;
+};
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
