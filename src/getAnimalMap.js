5fc71aab507ac5e7c5967ef36bf27676366932ca
luc0liv <luoliveiradacunha@gmail.com>
2022-10-04 15:27:05 -0300

    Requisito 14: parte 1

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const { species } = data;
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const getSpeciesLocation = () => {
+  const speciesLocations = locations.map((location) => (
+    {
+      [location]: species.filter((animal) => animal.location === location)
+        .map((animal) => animal.name),
+    }
+  ));
+  return speciesLocations.reduce((obj, item) => Object.assign(obj, item), {});
+};
+
+const getAnimalMap = (options) => {
+  if (!options) {
+    return getSpeciesLocation();
+  }
+};
 
 module.exports = getAnimalMap;
